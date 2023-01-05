305f75310a1bfc53e41e97f48770170480d626d1
Noelle Huchette <noelle.huchette@gmail.com>
2023-01-05 13:51:40 -0500

    setting up file structure

---
+++ src/client/components/Nav/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './style.css';
+
+function Nav() {
+  let navigate = useNavigate();
+  return (
+    <nav>
+      <div className="menu-head">
+        <button onClick={() => navigate('/')}>Noelle Huchette</button>
+      </div>
+      <div className="menu-pages">
+        <button
+          onClick={() => {
+            navigate('/projects');
+          }}
+        >
+          PROJECTS
+        </button>
+        <p>||</p>
+        <button
+          onClick={() => {
+            navigate('/contact');
+          }}
+        >
+          CONTACT
+        </button>
+        <p>||</p>
+        <a href="/NoelleHuchetteResume.pdf" download="NoelleHuchetteResume">
+          <button>RESUME</button>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
